30c78b39df8679869de105a218f384dc6ab5f458
Rukhshi Faheem <rukhshi.faheem@gmail.com>
2024-08-05 11:04:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// document.querySelector('.image-container').addEventListener('mouseover', function() {
+//     this.querySelector('.img-fluid').style.opacity = '1';
+//     this.querySelector('.overlay').style.opacity = '1';
+//   });
+  
+//   document.querySelector('.image-container').addEventListener('mouseout', function() {
+//     this.querySelector('.img-fluid').style.opacity = '0.7';
+//     this.querySelector('.overlay').style.opacity = '0';
+//   });
